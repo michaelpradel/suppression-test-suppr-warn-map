7178e728d86a1113f9b4d6b56b92be3b1befb813
Michael Pradel <michael@binaervarianz.de>
2023-08-24 10:52:32 +0200

    Adding code with three suppressions

---
+++ hello.py
@@ -0,0 +1,11 @@
+import does.nt.exist  # pylint: disable=import-error
+
+def toy():
+    """toy example"""
+    # pylint: disable=undefined-variable
+    var_a = 23
+    var_b = 42
+    var_c = var_a + var_b + var_x
+    var_d = var_c - var_y
+    # pylint: disable=bad-except-order
+    return var_d
